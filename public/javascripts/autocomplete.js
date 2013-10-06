f703279bf7849513d4291f09eb64faba047fab7b
Zhongming Chen <jeremy001181@gmail.com>
2013-10-06 23:28:47 +0100

    fixed a bug that autocomplete drop down item cannot be selected

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -7,8 +7,10 @@ $( "#tbx-search" ).autocomplete({
 	var label = item.label.replace(new RegExp("^(" + this.term + ")", "i"), "<b>$1</b>");
 	label = label.replace(new RegExp("\\b("+ this.term + ")", "i"), "<b>$1</b>");
 	
-    return $("<li></li>")
-    .append(label)
+//	<a id="ui-id-5" class="ui-corner-all" tabindex="-1">Charlie Ballentine</a>
+	
+    return $("<li class='ui-menu-item' role='presentation'></li>")
+    .append($('<a class="ui-corner-all"></a>').append(label))
     .data("ui-autocomplete-item", item)
     .appendTo(ul);
 };
