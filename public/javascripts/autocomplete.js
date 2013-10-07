240547f997df03bda533e38c970d9e4b2ccb10c9
Zhongming Chen <jeremy001181@gmail.com>
2013-10-07 22:00:32 +0100

    performance enhancement on autocomplete search , reuse previous requested data when the new search term match the old search term and previous request data count is less than defined limit

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,10 +1,38 @@
+//todo: refactoing this to be a plugin
 $( "#tbx-search" ).autocomplete({
-	source: "/autocomplete",
+	source: function(req, res)
+	{
+		var root = this;
+		var term = req.term.trim();
+		
+		// todo : refactoring this to be a function
+        if (root.options.preRequestedData && 
+        	new RegExp("^" + root.options.preRequestedTerm, "i").test(term) &&
+        	root.options.preRequestedData.length < 6)
+    	{
+        	res($.grep( root.options.preRequestedData, function( item ){
+                return new RegExp("\\b" + term, "ig").test( item );
+            }));
+        	
+        	return;
+    	}
+		
+        $.ajax({
+        	url: "/autocomplete?term=" + term,
+    	}).done(function( data ) {
+    		root.options.preRequestedData = data;
+    		root.options.preRequestedTerm = term;
+    		res(data);
+    	});
+	},
 	search: function( event, ui ) {
-		console.log(event);
+		
 	}	
 }).data("ui-autocomplete")._renderItem = function (ul, item) {
-	var label = item.label.replace(new RegExp("\\b("+ this.term + ")", "ig"), "<b>$1</b>");
+	
+	var term =  this.term.trim();
+	
+	var label = item.label.replace(new RegExp("\\b("+ term + ")", "ig"), "<b>$1</b>");
 	
 //	<a id="ui-id-5" class="ui-corner-all" tabindex="-1">Charlie Ballentine</a>
 	
