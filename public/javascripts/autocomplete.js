0cb2e5e5abe38dc6a9f52663475866f9964a0048
Zhongming Chen <jeremy001181@gmail.com>
2013-10-07 20:32:22 +0100

    Fixed autocomplete regex issue

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -4,13 +4,12 @@ $( "#tbx-search" ).autocomplete({
 		console.log(event);
 	}	
 }).data("ui-autocomplete")._renderItem = function (ul, item) {
-	var label = item.label.replace(new RegExp("^(" + this.term + ")", "i"), "<b>$1</b>");
-	label = label.replace(new RegExp("\\b("+ this.term + ")", "i"), "<b>$1</b>");
+	var label = item.label.replace(new RegExp("\\b("+ this.term + ")", "ig"), "<b>$1</b>");
 	
 //	<a id="ui-id-5" class="ui-corner-all" tabindex="-1">Charlie Ballentine</a>
 	
     return $("<li class='ui-menu-item' role='presentation'></li>")
-    .append($('<a class="ui-corner-all"></a>').append(label))
-    .data("ui-autocomplete-item", item)
-    .appendTo(ul);
+	    .append($('<a class="ui-corner-all"></a>').append(label))
+	    .data("ui-autocomplete-item", item)
+	    .appendTo(ul);
 };
